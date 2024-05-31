2b29eadab97fc7b08cf5ba5e95981bb581f229a2
Ademola Taiwo <taiwoademola465@gmaile.com>
2024-05-31 12:32:21 +0100

    added about section, mobile responsive

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import "../style/style.css";
 // components
 import "../style/components/header.css";
 import "../style/components/hero.css";
+import "../style/components/about.css";
 
 // utility
 import "../style/utility.css";
