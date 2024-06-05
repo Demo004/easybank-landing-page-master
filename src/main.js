bd72107563519bd33f4f7ae218b825d593dc7695
Ademola Taiwo <taiwoademola465@gmaile.com>
2024-06-05 03:18:33 +0100

    restarted the site, had some errors 😭

--- src/main.js
+++ src/main.js
@@ -7,8 +7,16 @@ import "../style/style.css";
 // components
 import "../style/components/header.css";
 import "../style/components/hero.css";
-import "../style/components/about.css";
-import "../style/components/blog.css";
 
 // utility
 import "../style/utility.css";
+
+//###  ham menu-effect
+document.addEventListener("DOMContentLoaded", function () {
+  const hamIcon = document.querySelector(".ham__icon");
+  const headerMenu = document.querySelector(".header__menu");
+
+  hamIcon.addEventListener("click", function () {
+    headerMenu.classList.toggle("active");
+  });
+});
