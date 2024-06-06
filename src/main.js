b42a840852fd9fd8d4f8c410c3c96d0ef5a43acb
Ademola Taiwo <taiwoademola465@gmaile.com>
2024-06-06 04:08:36 +0100

    mobile-done

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ import "../style/style.css";
 // components
 import "../style/components/header.css";
 import "../style/components/hero.css";
+import "../style/components/about.css";
+import "../style/components/blog.css";
+import "../style/components/footer.css";
 
 // utility
 import "../style/utility.css";
@@ -18,5 +21,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
   hamIcon.addEventListener("click", function () {
     headerMenu.classList.toggle("active");
+  });
+});
+
+
+// ###header scroll shadow effects
+window.addEventListener('scroll', function() {
+  var header = document.getElementById('header');
+  if (window.scrollY > 0) {
+      header.classList.add('shadow');
+  } else {
+      header.classList.remove('shadow');
+  }
+});
+
+
+// ### scroll behavior
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const target = document.querySelector(this.getAttribute("href"));
+
+      if (target) {
+        target.scrollIntoView({
+          behavior: "smooth",
+        });
+      }
+    });
   });
 });
